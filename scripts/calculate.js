aa07b0bc47f6bd69808bf5f1a24ba6434a6fb703
Dave Mulholland <dgmulholland@gmail.com>
2020-03-31 12:19:58 +0100

    basic page

---
+++ scripts/calculate.js
@@ -0,0 +1,13 @@
+wolfstagramCalculateButton.onclick = () => {
+    let curr_followers = parseInt(document.forms["wolfstagramCalculatorForm"]["curr_followers"].value);
+    let cha = parseInt(document.forms["wolfstagramCalculatorForm"]["cha"].value);
+    let d20 = parseInt(document.forms["wolfstagramCalculatorForm"]["d20"].value);
+    let d4 = parseInt(document.forms["wolfstagramCalculatorForm"]["d4"].value);
+
+    // alert(curr_followers + ", " + cha + ", " + d20 + ", " +  d4);
+    let likes = Math.floor(((d20 + cha)/100) * curr_followers);
+    let new_followers = Math.floor(((d4 * 10)/100) * likes);
+
+    alert("Likes: " + likes + "\n" + "New Followers: " + new_followers + "\n" + "New Total Followers: " + (curr_followers + new_followers))
+
+};
